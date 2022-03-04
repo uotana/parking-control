4dca5336a16d9b3834f5d99222fb4e2fa7cc2bd3
Uotana Carvalho <uotanacarvalho@gmail.com>
2022-03-03 21:27:18 -0300

    first commit

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDTO.java
@@ -0,0 +1,30 @@
+package com.api.parkingcontrol.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class ParkingSpotDTO {
+
+    @NotBlank
+    private String parkingSpotNumber;
+    @NotBlank
+    @Size(max = 7)
+    private String licensePlateCar;
+    @NotBlank
+    private String brandCar;
+    @NotBlank
+    private String modelCar;
+    @NotBlank
+    private String colorCar;
+    @NotBlank
+    private String responsibleName;
+    @NotBlank
+    private String apartament;
+    @NotBlank
+    private String block;
+}
